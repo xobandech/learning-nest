64fbb61cdc3b394a59b8a58480807260ab181b30
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-12-06 15:11:13 +0100

    Add Fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
+import { NestFastifyApplication } from '@nestjs/platform-fastify/interfaces';
+import { env } from 'process';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  console.log(env.MONGO_URI)
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   await app.listen(3000);
 }
 bootstrap();
